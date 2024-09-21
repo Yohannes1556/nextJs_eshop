67040c10b4d3a7b55cb299fca9b965ef6bf2feb9
Yohannes1556 <johnaddisu1556@gmail.com>
2024-09-21 18:48:32 +0300

    home banner and footer added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import Image from 'next/image'
+import Container from './components/Container'
+import HomeBanner from './components/HomeBanner'
 
 export default function Home() {
   return (
-    <div>E-shop</div>
+    <Container>
+      <div>
+        <HomeBanner/>
+      </div>
+    </Container>
   )
 }
