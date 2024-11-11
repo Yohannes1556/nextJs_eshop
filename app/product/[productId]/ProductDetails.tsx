60f863ce0e3c7e2bc2514be73b8829c7275c3fbc
Yohannes1556 <johnaddisu1556@gmail.com>
2024-11-11 18:34:47 +0300

    product detail page done

--- app/product/[productId]/ProductDetails.tsx
+++ app/product/[productId]/ProductDetails.tsx
@@ -6,7 +6,9 @@ import SetColor from "@/app/components/products/SetColor";
 import SetQuantity from "@/app/components/products/SetQuantity";
 import { useCart } from "@/hooks/useCart";
 import { Rating } from "@mui/material";
-import { useCallback, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
+import { MdCheckCircle } from "react-icons/md";
 
 interface ProductDetailsProps {
   product: any;
@@ -33,7 +35,8 @@ const Horizontal = () => {
   return <hr className="w-[30%] my-2" />;
 };
 const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
-  const {cartTotalQty} = useCart()
+  const { handleAddProductToCart, cartProducts } = useCart();
+  const [isProductInCart, setIsProductInCart] = useState(false);
   const [cartProduct, setCartProduct] = useState<CartProductType>({
     id: product.id,
     name: product.name,
@@ -45,6 +48,23 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
     price: product.price,
   });
 
+  const router = useRouter();
+
+  console.log(cartProducts);
+
+  useEffect(() => {
+    setIsProductInCart(false);
+
+    if (cartProducts) {
+      const existingIndex = cartProducts.findIndex(
+        (item) => item.id === product.id
+      );
+
+      if (existingIndex > -1) {
+        setIsProductInCart(true);
+      }
+    }
+  }, [cartProducts]);
   const productRating =
     product.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) /
     product.reviews.length;
@@ -59,20 +79,24 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
   );
 
   const handleQtyDecrease = useCallback(() => {
-    if(cartProduct.quantity === 1) return
+    if (cartProduct.quantity === 1) return;
     setCartProduct((prev) => {
       return { ...prev, quantity: prev.quantity-- };
     });
   }, [cartProduct]);
   const handleQtyIncrease = useCallback(() => {
     setCartProduct((prev) => {
-      return { ...prev, quantity: prev.quantity+ 1};
+      return { ...prev, quantity: prev.quantity + 1 };
     });
   }, [cartProduct]);
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-      <ProductImage cartProduct={cartProduct} product={product} handleColorSelect={handleColorSelect}/>
+      <ProductImage
+        cartProduct={cartProduct}
+        product={product}
+        handleColorSelect={handleColorSelect}
+      />
       <div className="flex flex-col gap-1 text-slate-500 text-sm">
         <h2 className="text-3xl font-medium text-slate-700">{product.name}</h2>
         <div className=" flex items-center gap-2">
@@ -95,25 +119,48 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
           {product.inStock ? "In stock " : "Out of Stock"}
         </div>
         <Horizontal />
-        <SetColor
-          cartProduct={cartProduct}
-          images={product.images}
-          handleColorSelect={handleColorSelect}
-        />
 
-        <Horizontal />
-        <div>
-          <SetQuantity
-            cartproduct={cartProduct}
-            handleQtyIncrease={handleQtyIncrease}
-            handleQtyDecrease={handleQtyDecrease}
-          />
-        </div>
-        <Horizontal />
-        <div className="max-w-[300px]">
-          <Button label="Add To Cart" onClick={()=>{}}
-          />
-        </div>
+        {isProductInCart ? (
+          <>
+            <p className="mb-2 text-slate-500 flex items-center gap-1">
+              <MdCheckCircle className="text-teal-400" size={20} />
+              <span>Product added to cart</span>
+            </p>
+            <div className="max-w-[300px]">
+              <Button
+                label="View Cart"
+                outline
+                onClick={() => {
+                  router.push("/cart");
+                }}
+              />
+            </div>
+          </>
+        ) : (
+          <>
+            <SetColor
+              cartProduct={cartProduct}
+              images={product.images}
+              handleColorSelect={handleColorSelect}
+            />
+
+            <Horizontal />
+            <div>
+              <SetQuantity
+                cartproduct={cartProduct}
+                handleQtyIncrease={handleQtyIncrease}
+                handleQtyDecrease={handleQtyDecrease}
+              />
+            </div>
+            <Horizontal />
+            <div className="max-w-[300px]">
+              <Button
+                label="Add To Cart"
+                onClick={() => handleAddProductToCart(cartProduct)}
+              />
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
--- hooks/useCart.tsx
+++ hooks/useCart.tsx
@@ -1,9 +1,10 @@
 import { CartProductType } from "@/app/product/[productId]/ProductDetails";
-import { createContext, useContext, useState } from "react";
+import { createContext, use, useCallback, useContext, useState } from "react";
 
 type CartContextType = {
   cartTotalQty: number;
-  cartProducts: CartProductType
+  cartProducts: CartProductType[] | null;
+  handleAddProductToCart: (product: CartProductType) => void
 };
 
 export const CartContext = createContext<CartContextType | null>(null);
@@ -14,8 +15,25 @@ interface Props {
 
 export const CartContextProvider = (props: Props) => {
   const [cartTotalQty, setCartTotalQty] = useState(0);
+  const [cartProducts, setCartProducts] = useState<CartProductType[] | null>(null)
 
-  const value = { cartTotalQty };
+
+  const handleAddProductToCart = useCallback((product :CartProductType) => {
+
+    setCartProducts((prev) => {
+      let updatedCart;
+
+      if(prev){
+        updatedCart = [...prev, product]
+      }else{
+        updatedCart = [product]
+      }
+
+      return updatedCart;
+    })
+    
+  }, [])
+  const value = { cartTotalQty, cartProducts, handleAddProductToCart };
   return <CartContext.Provider value={value} {...props} />;
 };
 
