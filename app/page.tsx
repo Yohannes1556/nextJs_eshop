0d18356352c19a1c5548a681d57307282939b886
Yohannes1556 <johnaddisu1556@gmail.com>
2025-01-02 17:51:44 +0300

    error fix

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
         <HomeBanner />
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-        {products.map((product: any) => {
-          return <ProductCard data={product} />;
+        {products.map((product: any, key:any) => {
+          return <ProductCard data={product} key={key}/>;
         })}
       </div>
     </Container>
